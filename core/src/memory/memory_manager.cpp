4567f8f52c96436c63c191271129bfcd66da161d
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-02-05 12:17:05 +0100

    Starting to set up an explicit memory management for (GPU) devices.

---
+++ core/src/memory/memory_manager.cpp
@@ -0,0 +1,43 @@
+/** Detray Data Model project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Local include(s).
+#include "detraydm/memory/memory_manager.hpp"
+#include "detraydm/memory/memory_manager_interface.hpp"
+
+namespace detraydm {
+
+   /// The destructor needs to be explicitly implemented, so that
+   /// @c detraydm::memory_manager_interface could be forward-declared in
+   /// @c memory_manager.hpp.
+   memory_manager::~memory_manager() {
+
+   }
+
+   memory_manager& memory_manager::instance() {
+
+      static memory_manager instance;
+      return instance;
+   }
+
+   void memory_manager::set( std::unique_ptr< memory_manager_interface > mgr ) {
+
+      m_mgr = std::move( mgr );
+      return;
+   }
+
+   memory_manager_interface& memory_manager::get() const {
+
+      return *m_mgr;
+   }
+
+   memory_manager::memory_manager()
+   : m_mgr() {
+
+   }
+
+} // namespace detraydm
